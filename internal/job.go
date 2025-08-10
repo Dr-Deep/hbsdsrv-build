12287d44aaade3724fdff31c53d658fe20ffa1c1
Dr-Deep <kirby.dpx@proton.me>
2025-08-10 18:12:54 +0200

    fix

--- internal/job.go
+++ internal/job.go
@@ -5,7 +5,8 @@ import (
 	"os/exec"
 )
 
-const DefaultShell = "/bin/sh -euxc"
+const DefaultShell = "/bin/sh"
+const DefaultShellArgs = "-euxc"
 
 type Job interface {
 	Run(*Builder) error
@@ -16,11 +17,14 @@ func RunCommand(args []string) (context.CancelFunc, *exec.Cmd) {
 	// ctx, cancel := context.WithCancel(context.Background())
 	// defer cancel()
 
+	var argv = []string{DefaultShellArgs}
+	argv = append(argv, args...)
+
 	ctx, cancel := context.WithCancel(context.Background())
 	var cmd = exec.CommandContext(
 		ctx,
 		DefaultShell,
-		args...,
+		argv...,
 	)
 
 	return cancel, cmd
